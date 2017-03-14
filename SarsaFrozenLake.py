ae3838d3ef4d689e1297381a1fd3d81d06e554bf
Polina Koval <kovalp@yandex-team.ru>
2017-03-14 17:31:35 +0500

    sarsa lambda v.2

--- SarsaFrozenLake.py
+++ SarsaFrozenLake.py
@@ -5,37 +5,47 @@ env = gym.make('FrozenLake-v0')
 
 
 class QLearningAgent(object):
-    def __init__(self, action_space, observation_space):
+    def __init__(self, action_space, observation_space, DF, LF, lam, er, er_limit):
         self.action_space = action_space
         self.observation_space = observation_space
-        self.qmatrix = [[0 for _ in range(action_space.n)] for _ in range(observation_space.n)]
+        self.qmatrix = [[0.4 for _ in range(action_space.n)] for _ in range(observation_space.n)]
         self.e = numpy.zeros((self.observation_space.n, self.action_space.n))
         self.last_state = 0
         self.last_action = -1
-        self.DF = 0.99
-        self.LF = 0.1
-        self.lam = 0.2
+        self.DF = DF
+        self.LF = LF
+        self.lam = lam
+        self.er = er
+        self.er_limit = er_limit
+
+    def reset(self):
+        self.e = numpy.zeros((self.observation_space.n, self.action_space.n))
+        self.last_state = 0
+        self.last_action = -1
 
     def act(self, state, reward, done, episode):
         action = self.get_action(state, episode)
         if self.last_action != -1:
-            self.recalculate(state, reward, action)
+            self.recalculate(state, reward, done, action)
         self.last_action = action
         self.last_state = state
-        if done:
-            self.e = numpy.zeros((self.observation_space.n, self.action_space.n))
+
         return action
     
     def get_action(self, state, t):
         max_action = numpy.argmax(self.qmatrix[state])
-        cond = random.uniform(0, 1) > float(100)/(t + 1)
+        cond = random.uniform(0, 1) > self.er or t > self.er_limit
         return max_action if cond else random.randint(0, self.action_space.n - 1)
 
-    def recalculate(self, state, reward, next_action):
+    def recalculate(self, state, reward, done, next_action):
         old_value = self.qmatrix[self.last_state][self.last_action]
         next_value = self.qmatrix[state][next_action]
         delta = reward + self.DF * next_value - old_value
         self.e[self.last_state][self.last_action] += 1
+
+        if done:
+            delta = reward - old_value
+
         for state_ in range(self.observation_space.n):
             for action_ in range(self.action_space.n):
                 self.qmatrix[state_][action_] += self.LF * self.e[state_][action_] * delta
@@ -43,15 +53,13 @@ class QLearningAgent(object):
 
 
 def run(agent, count=10000):
-    episode_count = count
-    wins = 0
     last100 = []
     average_score = 0
     i_episode = 0
-    while True:
+    while i_episode < count:
         i_episode += 1
+        agent.reset()
         observation = env.reset()
-        agent.last_action = -1
         reward = 0
         done = False
         while True:
@@ -63,19 +71,14 @@ def run(agent, count=10000):
                     last100.pop(0)
                     average_score = numpy.mean(last100)
                 agent.act(observation, reward, done, i_episode)
-                if reward == 1:
-                    wins += 1
                 break
 
         if average_score >= 0.78:
             print(average_score, i_episode)
             break
-    # print(wins, episode_count)
-    # print(float(wins)/episode_count)
-    # policy = map(numpy.argmax, agent.qmatrix)
-    # print(policy)
-    # print(agent.qmatrix)
 
 if __name__ == '__main__':
-    agent = QLearningAgent(env.action_space, env.observation_space)
-    run(agent)
+    for i in xrange(0, 10):
+        print "game {}".format(i),
+        agent = QLearningAgent(env.action_space, env.observation_space, 0.99, 0.1, 0.2, 0.1, 100)
+        run(agent)
